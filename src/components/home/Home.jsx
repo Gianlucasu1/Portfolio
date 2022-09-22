be5be3645b1fc50488d56af767edf07797df4489
Gianluca <gianluca@MacBook-Pro-de-Gianluca.local>
2022-09-22 11:19:01 -0500

    Home component almost done 100%

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -2,19 +2,20 @@ import React from 'react'
 import './home.css'
 import {Social} from "../home/Social"
 import {Data} from "../home/Data"
+import {ScrollDown} from "../home/ScrollDown"
 
 export const Home = () => {
     return (
         <section className="home section" id="home">
 
-            <div className="home__containter container grid">
+            <div className="home__container container grid">
 
                 <div className="home__content grid">
-
                     <Social />
-                    <img></img>
+                    <div className="home__img"></div>
                     <Data />
                 </div>
+                <ScrollDown />
             </div>
         </section>
     )
